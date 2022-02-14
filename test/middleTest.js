b1549f028cb895d8669efc2a1307336f8ca28b18
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-14 10:21:34 -0500

    Replaced original tests using mocha/chai in middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,26 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const {assert} = require('chai');
 const middle = require('../middle');
 
-const original = [1, 2, 3];
-middle(original);
-
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle(['a', undefined]), []);
-assertArraysEqual(original, [1, 2, 3]);
+describe('#middle', ()=>{
+  it('[1, 2, 3] should return [2]', ()=>{
+    const actual = middle([1, 2, 3]);
+    const expected = [2];
+    assert.sameOrderedMembers(actual, expected);
+  });
+  it('[1, 2, 3, 4] should return [2, 3]', ()=>{
+    const actual = middle([1, 2, 3, 4]);
+    const expected = [2, 3];
+    assert.sameOrderedMembers(actual, expected);
+  });
+  it('[1] should return []', ()=>{
+    const actual = middle([1]);
+    const expected = [];
+    assert.sameOrderedMembers(actual, expected);
+  });
+  it("Shouldn't mutate the original array", ()=>{
+    const original = [1, 2, 3];
+    const expected = [1, 2, 3];
+    middle(original);
+    assert.sameOrderedMembers(original, expected);
+  });
+});
