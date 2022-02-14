fbbbea57765e327553c277cadd997dd950a49f7e
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-14 14:43:48 -0500

    exported findKey, removed old tests from findKey

---
+++ test/findKeyTest.js
@@ -0,0 +1,19 @@
+/*
+const cities = {
+  windsor: {
+    province: 'ON'
+  },
+  montreal: {
+    province: 'QC'
+  },
+  calgary: {
+    province: 'AB'
+  },
+  vancouver: {
+    province: 'BC'
+  }
+};
+
+assertEqual(findKey(cities, city => city.province === 'AB'), 'calgary');
+assertEqual(findKey(cities, city => city.province === 'SK'), undefined);
+*/
