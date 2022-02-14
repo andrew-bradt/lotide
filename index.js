4c0e0aca7b66977a7e351fc1e73fc6864b3ee29c
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-14 14:08:50 -0500

    Retry previous commit

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
 const head = require('./head');
 const middle = require('./middle');
 const tail = require('./tail');
 
 module.exports = {
-  assertArraysEqual,
-  assertEqual,
-  assertObjectsEqual,
   countLetters,
-  eqArrays,
-  eqObjects,
   head,
   middle,
   tail,
