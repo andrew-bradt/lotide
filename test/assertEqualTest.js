dadd352b3a19a5680d71565905dd6c4757c10f79
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-14 14:14:50 -0500

    Removed tests which were not written with mocha/chai

--- test/assertEqualTest.js
+++
@@ -1,6 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual("Dog", "Dog");
-assertEqual(1, 1);
-assertEqual(99, 101);
--- test/eqArraysTest.js
+++
@@ -1,8 +0,0 @@
-const assertEqual = require('../assertEqual');
-const eqArrays = require('../eqArrays');
-
-assertEqual(eqArrays(['a', 2], ['a', 2]), true);
-assertEqual(eqArrays([false, true], [false, true]), true);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays(['1', 1], [1, 1]), false);
-assertEqual(eqArrays([null], []), false);
