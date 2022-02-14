a4dec0cabbbf628246a1d510a1e6ea8537276ce3
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-14 14:28:39 -0500

    Added import and export statements to index.js for all utility functions

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+// Functions with Automated Tests
 const countLetters = require('./countLetters');
 const head = require('./head');
 const middle = require('./middle');
 const tail = require('./tail');
+
+// Functions with Old Tests
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const index = require('./index');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
   countLetters,
   head,
   middle,
   tail,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  index,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
