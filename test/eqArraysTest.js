f1a2295133ada89ab175d40d7d521f94735c45e7
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-15 15:43:16 -0500

    Added additional test - return false if arrays are different lengths

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,6 +2,11 @@ const {assert} = require('chai');
 const eqArrays = require('../eqArrays');
 
 describe('#eqArrays', ()=>{
+  it('[1] and [1, 2] should return false', ()=>{
+    const arr1 = [1];
+    const arr2 = [1, 2];
+    assert.isFalse(eqArrays(arr1, arr2));
+  });
   it('[[2, 3], [4]] and [[2, 3], [4]] should return true', ()=>{
     const arr1 = [[2, 3], [4]];
     const arr2 = [[2, 3], [4]];
