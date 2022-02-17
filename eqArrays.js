5603c07700f61175097f813a25482afc6a1f0c75
Andrew Bradt <andrewjbradt@gmail.com>
2022-02-16 20:59:07 -0500

    Refactored eqArrays to use Recursion for any number of nested arrays of primitive types

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,10 @@
-const eqArrays = (array1, array2) => {
-  let i = 0;
-  if (array1.length === array2.length) {
-    while (i < array1.length) {
-      if (array1[i] !== array2[i]) {
-        break;
-      }
-      i++;
-    }
-  }
-  return i === array1.length;
-};
+const areBothArrays = (el1, el2) => Array.isArray(el1) && Array.isArray(el1) === Array.isArray(el2);
 
+const eqArrays = (el1, el2) => {
+  const [poppedEl1, poppedEl2] = [el1.pop(), el2.pop()];
+  if (areBothArrays(poppedEl1, poppedEl2) && poppedEl1.length === poppedEl2.length) {
+    return eqArrays(el1, el2) && eqArrays(poppedEl1, poppedEl2);
+  }
+  return poppedEl1 === poppedEl2;
+};
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -17,9 +17,35 @@ describe('#eqArrays', ()=>{
     const arr2 = [[2, 3], [4, 5]];
     assert.isFalse(eqArrays(arr1, arr2));
   });
+  it('[[2, 3], [4, 6]] and [[2, 3], [4, 5]] should return false', ()=>{
+    const arr1 = [[2, 3], [4, 6]];
+    const arr2 = [[2, 3], [4, 5]];
+    assert.isFalse(eqArrays(arr1, arr2));
+  });
+  it('[[4], [2, 3]] and [[4, 5], [2, 3]] should return false', ()=>{
+    const arr1 = [[4], [2, 3]];
+    const arr2 = [[4, 5], [2, 3]];
+    assert.isFalse(eqArrays(arr1, arr2));
+  });
   it('[[2, 3], [4]] and [[2, 3], 4] should return false', ()=>{
     const arr1 = [[2, 3], [4]];
     const arr2 = [[2, 3], 4];
+    assert.isFalse(eqArrays(arr1, arr2));
+  });
+  it('[[1, 3], [4]] and [[2, 3], 4] should return false', ()=>{
+    const arr1 = [[1, 3], [4]];
+    const arr2 = [[2, 3], 4];
+    assert.isFalse(eqArrays(arr1, arr2));
+  });
+  it('[1, 12, 123, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]] and [1, 12, 123, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]] should return true', ()=>{
+    const arr1 = [1, 12, 123, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]];
+    const arr2 = [1, 12, 123, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]];
     assert.isTrue(eqArrays(arr1, arr2));
   });
+  it('[1, 12, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]] and [1, 12, 123, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]] should return false', ()=>{
+    const arr1 = [1, 12, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]];
+    const arr2 = [1, 12, 123, [1, 23, 1, [1, 2, 3], [1, 1, 1], [1]]];
+    assert.isFalse(eqArrays(arr1, arr2));
+  });
 });
+
